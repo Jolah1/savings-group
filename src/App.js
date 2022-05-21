75c44b31ffc7912c7887761d24b22b16b6781067
okunlade Jolade <jokunlade@gmail.com>
2022-05-21 16:09:02 +0100

    adding info

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import React from "react";
 function Savings (){
 
 const tierOne = () => {
-  console.log("1000 Naira")
+  console.log("10,00 Naira")
+  console.log("20,000 Naira")
+  console.log("30,000 Naira")
 }
 
   return(
 <div>
   <h1>Group Savings Web Application</h1>
   <button onClick>7% Intrest per week</button>
+  <button onClick>12% Intrest per week</button>
+  <button onClick>25% Intrest per week</button>
+
 </div>
+
+
   );
 }
 
