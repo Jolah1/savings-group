70b41f2b7827dcde9d438f3f517b2b3d981c6176
okunlade Jolade <jokunlade@gmail.com>
2022-05-21 16:02:50 +0100

    Added readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Savings (){
+
+const tierOne = () => {
+  console.log("1000 Naira")
+}
+
+  return(
+<div>
+  <h1>Group Savings Web Application</h1>
+  <button onClick>7% Intrest per week</button>
+</div>
   );
 }
 
-export default App;
+export default Savings;
